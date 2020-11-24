2c694bd0096d54b13066971d96ab7689dbed6431
Prithviraj Kadiyala <prithvirajkadiyala@gmail.com>
2020-11-24 15:22:06 -0600

    AppNavbar

---
+++ client/src/components/AppNavBar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
+} from 'reactstrap';
+
+class AppNavbar extends Component {
+    state = {
+        isOpen = false
+    }
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+        <div>
+            <Navbar>
+                
+            </Navbar>
+        </div>
+    }
+}
+
+
+
+export default AppNavbar;
